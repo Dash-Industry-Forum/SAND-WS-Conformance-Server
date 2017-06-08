c1acb604f288f2dbc369549b3c8eb131b0cf524c
Emmanuel Thomas <emmanuel.thomas@tno.nl>
2017-06-08 18:19:27 +0200

    [GO]

---
+++ README.md
@@ -0,0 +1,23 @@
+# SAND WS conformance server
+
+The SAND WebSocket conformance server implements a conformance server for 
+ISO/IEC 23009-5 Server And Network-asssited DASH (SAND).
+
+The server validates the incoming SAND messages as well as the protocols used by
+a SAND client.
+
+## Requirements
+
+- [pip](https://pip.pypa.io/en/stable/)
+
+## Installation
+
+```
+pip install -r requirements.txt
+```
+
+## Usage
+
+```
+python server.py
+```
---
+++ server.py
@@ -0,0 +1,84 @@
+"""
+SAND WS conformance server.
+
+This implements a conformance server for ISO/IEC 23009-5 SAND.
+It validates the incoming SAND messages as well as the protocols used by
+a SAND client.
+
+Copyright (c) 2016-, ISO/IEC JTC1/SC29/WG11
+All rights reserved.
+
+See AUTHORS for a full list of authors.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+* Neither the name of the ISO/IEC nor the
+names of its contributors may be used to endorse or promote products
+derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+from autobahn.twisted.websocket import WebSocketServerProtocol
+import logging
+
+logging.basicConfig(filename='debug.log', level=logging.DEBUG, filemode='w')
+
+class SandConformanceServer(WebSocketServerProtocol):
+    """
+    Implements a WebSocket SAND conformance server that validates
+    incoming SAND messages.
+    """
+
+    def onConnect(self, request):
+        logging.info("Client connecting: {0}".format(request.peer))
+
+    def onOpen(self):
+        logging.debug("WebSocket connection open.")
+
+    def onMessage(self, payload, isBinary):
+        if isBinary:
+            logging.debug(
+                "Binary message received: {0} bytes".format(len(payload)))
+        else:
+            logging.debug(
+                "Text message received: {0}".format(payload.decode('utf8')))
+
+        # echo back message verbatim
+        self.sendMessage(payload, isBinary)
+
+    def onClose(self, wasClean, code, reason):
+        logging.info("WebSocket connection closed: {0}".format(reason))
+
+def run():
+    """
+    Runs the server.
+    """
+    import sys
+
+    from twisted.python import log
+    from twisted.internet import reactor
+    log.startLogging(sys.stdout)
+
+    from autobahn.twisted.websocket import WebSocketServerFactory
+    factory = WebSocketServerFactory()
+    factory.protocol = SandConformanceServer
+
+    reactor.listenTCP(9000, factory)
+    reactor.run()
+
+if __name__ == '__main__':
+    run()
